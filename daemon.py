aedd2e0962e39e6ae7e5b34f6918143f8962b2f3
Mario Latif <32080032+mariolatiffathy@users.noreply.github.com>
2020-04-01 20:22:20 +0200

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -1,1 +1,12 @@
+import subproccess
+from flask import Flask, jsonify
+app = Flask(__name__)
 
+
+@app.route('/api')
+def api():
+    # TODO: Check authentication
+    return jsonify({})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8794)
