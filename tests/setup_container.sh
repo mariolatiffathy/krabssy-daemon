b1ea01980b1236555a64c00aae2fc1bdcc822b1a
Mario Latif <32080032+mariolatiffathy@users.noreply.github.com>
2020-04-05 14:56:19 +0200

    Update setup_container.sh

--- tests/setup_container.sh
+++ tests/setup_container.sh
@@ -2,6 +2,6 @@
 lxc-create -n fabitmanage-container1 -t centos
 lxc-stop -n fabitmanage-container1
 echo 'lxc.cgroup.cpu.shares = 2' >> /var/lib/lxc/fabitmanage-container1/config
-echo 'lxc.cgroup.memory.limit_in_bytes = 64MB' >> /var/lib/lxc/fabitmanage-container1/config
+echo 'lxc.cgroup.memory.limit_in_bytes = 64M' >> /var/lib/lxc/fabitmanage-container1/config
 lxc-start -n fabitmanage-container1 -d
 lxc-console -n fabitmanage-container1
