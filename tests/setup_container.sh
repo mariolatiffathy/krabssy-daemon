171a8d6f51b5b411e172a3a0af2ab22527ebe763
Mario Latif <32080032+mariolatiffathy@users.noreply.github.com>
2020-04-05 14:44:04 +0200

    Update setup_container.sh

--- tests/setup_container.sh
+++ tests/setup_container.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-lxc-create -n fabitmanage-container1 -t lxc-archlinux
+lxc-create -n fabitmanage-container1 -t centos
+lxc-stop -n fabitmanage-container1
+lxc config set fabitmanage-container1 limits.cpu 2
+lxc config set fabitmanage-container1 limits.memory 64MB
+lxc-start -n fabitmanage-container1 -d
+lxc-console -n fabitmanage-container1
