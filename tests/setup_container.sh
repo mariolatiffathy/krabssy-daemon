5588aa146a973862b6270ef4375f76c2fe27ba5f
Mario Latif <32080032+mariolatiffathy@users.noreply.github.com>
2020-04-07 09:44:28 +0200

    Update setup_container.sh

--- tests/setup_container.sh
+++ tests/setup_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-useradd -m -d /fabitmanage/daemon-data/container1 -p $(openssl passwd -1 FabitContainerPasswd) fabitmanage-container1
+useradd -m -d /home/fabitmanage/daemon-data/container1 -p $(openssl passwd -1 FabitContainerPasswd) fabitmanage-container1
