75ddcb9b8f2862543ad1ef3c669cff26888e8388
mariolatiffathy <mariolatif741@yandex.com>
2020-04-08 15:07:29 +0200

    Update installer

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 !/bin/bash
 echo "FabitManage Daemon v1.0-alpha Installer"
+echo "==========================================="
+echo "Downloading components..."
+git clone https://github.com/mariolatiffathy/fabitmanage-daemon.git fabitmanage-installer-tmp
+mkdir -p /fabitmanage-daemon && cp ./fabitmanage-installer-tmp/daemon.py /fabitmanage-daemon
+mkdir -p /fabitmanage-daemon/config && cp ./fabitmanage-installer-tmp/config/daemon.ini /fabitmanage-daemon/config
+echo "Removing temporary files..."
+rm -rf ./fabitmanage-installer-tmp/
