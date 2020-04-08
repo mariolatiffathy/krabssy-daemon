28ec82d41db5817321b95a162774b47004f1f061
mariolatiffathy <mariolatif741@yandex.com>
2020-04-08 14:46:08 +0200

    .

--- daemon.py
+++ daemon.py
@@ -1,6 +1,18 @@
 import subproccess
 import threading
+import configparser
+import time
 from flask import Flask, jsonify
+
+daemon_config = configparser.ConfigParser()
+daemon_config.read("/fabitmanage-daemon/config/daemon.ini")
+
+daemondb = mysql.connector.connect(
+  host=daemon_config['db']['host'],
+  user=daemon_config['db']['user'],
+  passwd=daemon_config['db']['password'],
+  database=daemon_config['db']['name']
+)
 
 app = Flask(__name__)
 
@@ -13,10 +25,19 @@ def api():
     return jsonify({"error": {"http_code": 405}}), 405
 
 def QueueManager():
-    # TODO: Fetch queue from the daemon DB and process it
+	while True:
+		time.sleep(1)
+	    queue_cursor = daemondb.cursor()
+	    queue_cursor.execute("SELECT * FROM queue WHERE being_processed = 0")
+	    result = queue_cursor.fetchall()
+	    if queue_cursor.rowcount > 0:
+	        for queue_item in result:
+		        update_being_processed = daemondb.cursor()
+		        update_being_processed.execute("UPDATE queue SET being_processed = 1 WHERE id = %s", (queue_item['id']))
+		        daemondb.commit()
     
 def PortBindingPermissions():
-    # TODO: Check binded ports' permissions
+    # TODO
 
 if __name__ == '__main__':
     # Define daemon threads
@@ -26,4 +47,4 @@ if __name__ == '__main__':
     QueueManager_t.start()
     PortBindingPermissions_t.start()
     # Start Flask server
-    app.run(host='0.0.0.0', port=4070)
+    app.run(host='0.0.0.0', port=int(daemon_config['server']['port']))
