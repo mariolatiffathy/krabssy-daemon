59cf42f332802cfd08a06e67405beb96a6077801
mariolatiffathy <mariolatif741@yandex.com>
2020-04-14 05:46:45 +0200

    No comment

--- daemon.py
+++ daemon.py
@@ -268,7 +268,7 @@ def server_power(server_id):
         if TMUX_SESSION_EXISTS == False:
             # No tmux session for the container is running... Start it now
             subprocess.check_output(['tmux', 'new', '-d', '-s', SERVER_CONTAINER_ID], cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
-            subprocess.check_output(('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER').split(' '), cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
+            subprocess.check_output('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER', cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, shell=True, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
             return jsonify({"success": {"http_code": 200, "description": "Server successfully started."}}), 200
         else:
             return jsonify({"error": {"http_code": 422, "description": "The server is already running."}}), 422
@@ -286,7 +286,7 @@ def server_power(server_id):
             pass
         try:
             subprocess.check_output(['tmux', 'new', '-d', '-s', SERVER_CONTAINER_ID], cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
-            subprocess.check_output(('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER').split(' '), cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
+            subprocess.check_output('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER', cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, shell=True, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
         except Exception as e:
             pass
         return jsonify({"success": {"http_code": 200, "description": "Server successfully restarted."}}), 200
@@ -556,6 +556,10 @@ def exit_handler():
             pass
     try:
         subprocess.check_output(['tmux', 'kill-server'])
+    except Exception as e:
+        pass
+    try:
+        subprocess.check_output(['killall', 'tmux'])
     except Exception as e:
         pass
     sys.exit()
