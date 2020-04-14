e4a9ebca20752f08c61026d2948222d2c19dc8e2
mariolatiffathy <mariolatif741@yandex.com>
2020-04-14 06:44:53 +0200

    No comment

--- daemon.py
+++ daemon.py
@@ -286,7 +286,7 @@ def server_power(server_id):
             pass
         try:
             subprocess.check_output(['tmux', 'new', '-d', '-s', SERVER_CONTAINER_ID], cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
-            subprocess.check_output('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER', cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, shell=True, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
+            subprocess.check_output('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER', shell=True, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
         except Exception as e:
             pass
         return jsonify({"success": {"http_code": 200, "description": "Server successfully restarted."}}), 200
