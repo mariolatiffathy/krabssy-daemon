f8d2bc27dc255061928dd399f3f9c2fbf9c76493
mariolatiffathy <mariolatif741@yandex.com>
2020-04-14 06:01:13 +0200

    No comment

--- daemon.py
+++ daemon.py
@@ -267,7 +267,8 @@ def server_power(server_id):
     if req_data['action'] == "start":
         if TMUX_SESSION_EXISTS == False:
             # No tmux session for the container is running... Start it now
-            subprocess.check_output('tmux new -d -s ' + SERVER_CONTAINER_ID, cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, shell=True, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
+            subprocess.check_output(['tmux', 'new', '-d', '-s', SERVER_CONTAINER_ID], cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
+            subprocess.check_output(['tmux', 'ls'], cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
             subprocess.check_output('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER', cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, shell=True, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
             return jsonify({"success": {"http_code": 200, "description": "Server successfully started."}}), 200
         else:
@@ -286,7 +287,6 @@ def server_power(server_id):
             pass
         try:
             subprocess.check_output(['tmux', 'new', '-d', '-s', SERVER_CONTAINER_ID], cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
-            time.sleep(0.5)
             subprocess.check_output('tmux send-keys -t ' + SERVER_CONTAINER_ID + '.0 "' + SERVER_STARTUP_COMMAND + '" ENTER', cwd="/home/fabitmanage/daemon-data/" + SERVER_CONTAINER_ID, shell=True, preexec_fn=AsUser(int(SERVER_CONTAINER_UID), int(SERVER_CONTAINER_GID)))
         except Exception as e:
             pass
@@ -476,7 +476,6 @@ def PortBindingPermissions():
                                 if conns.laddr.port == int(port):
                                     pid = int(proc.pid)
                                     pid_owner = proc.username()
-                                    break
                         except Exception as e:
                             pass
                     if "fabitmanage-" in pid_owner:
